b10ebd720c2f1e118d9b2c910692a66ffee0d8bc
Cynchronized <austin.thieu123@gmail.com>
2023-07-31 23:37:17 -0600

    made event listener to change color of grid

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ const grid = document.querySelector('.grid-container')
 grid.style.gridTemplateColumns = "repeat(16, 1fr)"
 grid.style.gridTemplateRows = "repeat(16, 1fr)"
 
-makeGrid()
+document.addEventListener('onload', makeGrid())
 
 
 /* Functions */
 function makeGrid() {
-
     for(let i=0; i<256; i++) {
         let box = document.createElement('div')
-        box.style.backgroundColor = 'white'
-        box.style.border = 'thin solid black'
+        box.classList.add('grid-box')
         grid.insertAdjacentElement('beforeend', box)
     }
-
 }
+
+document.addEventListener("mouseover", e => {
+
+    let square = e.target
+
+    if (square.parentNode === grid) {
+        square.style.backgroundColor = 'blue'
+    }
+    
+    console.log(square)
+
+})
